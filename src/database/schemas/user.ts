5d3d40c28bea7e572ed66dd4cbd8b8eefdb89e89
Wendell <wendellmorais3319@gmail.com>
2022-06-14 22:12:14 -0300

    add dotenv

--- src/database/schemas/user.ts
+++ src/database/schemas/user.ts
@@ -1,11 +1,12 @@
 import mysql from "mysql2/promise";
+import "dotenv/config";
 
 export const connectDB = async () => {
     const connection = await mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "Senhadoserver?",
-        database: "crud_db",
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     });
 
     console.log("Connected at MySQL");
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
+import "dotenv/config";
 
 const app = express();
+const PORT = process.env.PORT;
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log("Server is running at http://localhost:3001");
 });
